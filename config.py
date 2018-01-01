e2161c411492682247d8fdf2913467c8fff31848
Vagner Clementino <vagner.clementino@gmail.com>
2018-01-01 16:41:08 -0200

    Alteração do arquivo de configuração do sistema

--- config.py
+++ config.py
@@ -6,22 +6,25 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SQLALCHEMY_DATABASE_URI = 'postgresql://andrezaevagner:06012018@localhost:5432/andrezaevagner'
+
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = ''
 
 
 class StagingConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = ''
 
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://andrezaevagner:06012018@localhost:5432/andrezaevagner'
 
 
 class TestingConfig(Config):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = ''
