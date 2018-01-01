2cdcf52ec3ea6b801c5896bd43f0903f30fcd376
Vagner Clementino <vagner.clementino@gmail.com>
2018-01-01 17:21:17 -0200

    Alterado para aplicacao no heroku

--- model/RSVPModel.py
+++ model/RSVPModel.py
@@ -1,14 +1,14 @@
-from rsvp import db
+from app import db
 
 
 class RSVPModel(db.Model):
     __tablename__ = 'confirmacao_presenca'
-    __table_args__ = {"schema":"rsvp"}
+    __table_args__ = {"schema": "rsvp"}
 
     id_confirmacao_presenca = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String())
     email = db.Column(db.String())
-    evento= db.Column(db.String())
+    evento = db.Column(db.String())
     acompanhante = db.Column(db.String())
     observacao = db.Column(db.String())
 
